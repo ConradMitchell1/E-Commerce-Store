e887f1149ab76f67baae4c17f5818d57f496d02d
Conrad Mitchell <Conradmitchell@outlook.com>
2025-03-26 12:00:38 +0000

    Add project files.

---
+++ RazorPageApp/Program.cs
@@ -0,0 +1,43 @@
+
+
+namespace RazorPageApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.Configure<RouteOptions>(o =>
+            {
+                o.LowercaseUrls = true;
+                o.LowercaseQueryStrings = true;
+                o.AppendTrailingSlash = true;
+            });
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
